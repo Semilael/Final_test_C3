e7d767c3adcf7bddc83827a8672b76abe079b6e7
Semilael <ariana-ses97@yandex.ru>
2022-09-18 18:45:52 +0300

    Program file is changed

--- Program.cs
+++ Program.cs
@@ -1,19 +1,32 @@
 ﻿string[] array = 
 {
-    "Ice",
+    "Ice,",
     "Ice",
     "Baby",
-    "That's".
-    "The",
+    "That's",
+    "The_",
     "Motto",
+    "Btw",
     "You",
     "Belong",
     "With",
+    "&",
     "Me",
     "Lately I've been",
     "I've been loosing sleep",
-    "(-:",
     "(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧",
     "･ᴗ･"
+};
+
+void ArrayUnder3(string[] array)
+{
+    foreach (string item in array)
+    {
+        if(item.Length <= 3)
+        {
+            Console.WriteLine(item);
+        }
+    }
 }
 
+ArrayUnder3(array);
