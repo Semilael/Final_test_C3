f28661e20b892a0bc9beb09466a72ff06bdf6bff
Semilael <ariana-ses97@yandex.ru>
2022-09-18 16:55:00 +0300

    Array created

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿string[] array = 
+{
+    "Ice",
+    "Ice",
+    "Baby",
+    "That's".
+    "The",
+    "Motto",
+    "You",
+    "Belong",
+    "With",
+    "Me",
+    "Lately I've been",
+    "I've been loosing sleep",
+    "(-:",
+    "(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧",
+    "･ᴗ･"
+}
+
